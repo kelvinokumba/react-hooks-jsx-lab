cac8e0384bb69b12bb33fbf4c9c8a0a47fcec69c
Kelvin okumba <okumbakelvin@gmail.com>
2023-08-25 08:55:46 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id='about'>
+    <h2>About Me</h2>
+    <p>Passionate about technology and progress</p>
+    <img src={image} alt='I made this'></img>
+    About
+
+  </div>
 }
 
 export default About;
